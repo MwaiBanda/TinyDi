2bbffc3faed7c9f6ad6068b62cd47486547fb9ec
Mwai Banda <mwai.banda@cbn.org>
2022-11-14 03:05:44 -0600

    Module `refactor`

--- Sources/TinyDi/Module.swift
+++ Sources/TinyDi/Module.swift
@@ -10,18 +10,7 @@ import Foundation
 @resultBuilder
 public struct Module {
     public static func buildBlock<T>(_ dependencies: T...) -> Void {
-        
-        if let module = dependencies[0] as? (() -> DependencyModule) {
-            module().dependencies.forEach { dep in
-                Resolver.inject(dependency: dep.type, named: (dep.named.isEmpty ? String(
-                    String(describing: dep.self)
-                        .drop(while: { $0 != Character("<")})
-                        .split(separator: ">")
-                        .first?.dropFirst() ?? ""
-                    
-                ) : dep.named))
-            }
-        } else if let module = dependencies[0] as? DependencyModule {
+        if let module = dependencies[0] as? DependencyModule {
             module.dependencies.forEach { dep in
                 Resolver.inject(dependency: dep.type, named: (dep.named.isEmpty ? String(
                     String(describing: dep.self)
