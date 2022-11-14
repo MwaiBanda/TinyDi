3126cd83dd1a565d117e746fec0940bb26ff31d2
Mwai Banda <mwai.banda@cbn.org>
2022-11-14 01:04:27 -0600

    Module update

--- Sources/TinyDi/Module.swift
+++ Sources/TinyDi/Module.swift
@@ -10,8 +10,8 @@ import Foundation
 @resultBuilder
 public struct Module {
     public static func buildBlock<T>(_ dependencies: T...) -> Void {
-        let module = dependencies[0] as! DependencyModule
-        module.dependencies.forEach { dep in
+        let module = dependencies[0] as! () -> DependencyModule
+        module().dependencies.forEach { dep in
             Resolver.inject(dependency: dep.type, named: (dep.named.isEmpty ? String(
                 String(describing: dep.self)
                     .drop(while: { $0 != Character("<")})
