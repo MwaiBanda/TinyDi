306e22c1043d107cca0ae873b2bad389c3900b5e
Mwai Banda <mwai.banda@cbn.org>
2022-11-13 20:28:17 -0600

    Added public prefix phase::2

--- Tests/TinyDiTests/TinyDiTests.swift
+++ Tests/TinyDiTests/TinyDiTests.swift
@@ -6,6 +6,6 @@ final class TinyDiTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TinyDi().text, "Hello, World!")
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 }
