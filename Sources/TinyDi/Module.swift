ee640d62a0ff91497b49fb6ef2a74c4333ce8e13
Mwai Banda <mwai.banda@cbn.org>
2022-11-14 02:06:10 -0600

    Module Update

--- Sources/TinyDi/Module.swift
+++ Sources/TinyDi/Module.swift
@@ -10,15 +10,27 @@ import Foundation
 @resultBuilder
 public struct Module {
     public static func buildBlock<T>(_ dependencies: T...) -> Void {
-        let module = dependencies[0] as! (() -> DependencyModule)
-        module().dependencies.forEach { dep in
-            Resolver.inject(dependency: dep.type, named: (dep.named.isEmpty ? String(
-                String(describing: dep.self)
-                    .drop(while: { $0 != Character("<")})
-                    .split(separator: ">")
-                    .first?.dropFirst() ?? ""
+        
+        if let module = dependencies[0] as? (() -> DependencyModule) {
+            module().dependencies.forEach { dep in
+                Resolver.inject(dependency: dep.type, named: (dep.named.isEmpty ? String(
+                    String(describing: dep.self)
+                        .drop(while: { $0 != Character("<")})
+                        .split(separator: ">")
+                        .first?.dropFirst() ?? ""
                     
-            ) : dep.named))
+                ) : dep.named))
+            }
+        } else if let module = dependencies[0] as? DependencyModule {
+            module.dependencies.forEach { dep in
+                Resolver.inject(dependency: dep.type, named: (dep.named.isEmpty ? String(
+                    String(describing: dep.self)
+                        .drop(while: { $0 != Character("<")})
+                        .split(separator: ">")
+                        .first?.dropFirst() ?? ""
+                    
+                ) : dep.named))
+            }
         }
     }
 }
