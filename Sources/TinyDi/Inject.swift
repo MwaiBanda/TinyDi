49c5d817f5b6bcfbe0df1037831b56745aaf3536
Mwai Banda <mwai.banda@cbn.org>
2022-11-19 17:40:11 -0600

    Added inits Inject.swift

--- Sources/TinyDi/Inject.swift
+++ Sources/TinyDi/Inject.swift
@@ -15,19 +15,23 @@ public struct Inject<T>: DynamicProperty {
             TDi.resolve(named: named)
         }
         nonmutating set {
-            TDi.inject { _ in
-               new()
-            }
-            @Singleton
-            func new() {
-                newValue
-            }
-            
+            TDi.inject(dependency: newValue, named: String(describing: T.Type))
         }
     }
-    public var projectedValue: Binding<T> {
-         return Binding(get: { return self.wrappedValue }, set: { newValue in self.wrappedValue = newValue })
-     }
+    public init(wrappedValue value: T) {
+        self.wrappedValue = value
+        TDi.inject(dependency: value, named: String(describing: T.Type))
+        
+    }
+    
+    public init(initialValue value: T) {
+        self.wrappedValue = value
+        TDi.inject(dependency: value, named: String(describing: T.Type))
+    }
+    
+    public var projectedValue: StateObject<T> {
+        return Binding(get: { return self.wrappedValue }, set: { newValue in self.wrappedValue = newValue })
+    }
     public init(named: String = ""){
         self.named = named
     }
